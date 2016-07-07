6b94e906bb6b156714df141ea988b05c1c0dd793
Lcnvdl <programando.soft@gmail.com>
2016-07-07 15:54:06 -0300

    StateNav

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# statenav
+# Statenav
 Hyper-simple push/pop state navigation, compatible with Pjax.
+
+# Usage
+
+1. Import the script *statenav.js*
+
+2. Add a container
+```
+<div id="container"></div>
+```
+
+3. Initialize StateNav with the container
+```
+<script>
+	statenav.initialize("#container");
+</script>
+```
+
+4. Add the *statenav* class for all your links
+```
+<a href="/page1" class="statenav">My link</a>
+```
---
+++ statenav.js
@@ -0,0 +1,68 @@
+﻿(function ($) {
+
+    window.statenav = {
+
+        container: "body",
+
+        initialize: function (container) {
+            if (typeof container !== 'undefined') {
+				if(typeof container === 'string') {
+					container = $(container);
+				}
+                window.statenav.container = container;
+            }
+        },
+		
+		attachElements: function() {
+			var sn = window.statenav;
+
+            $("a.statenav,a.pjax").each(function() {
+				var e = $(this);
+				e.click(function(ev) {
+					ev.preventDefault();
+
+					var b = $(this),
+						link = b.attr("href");
+					history.pushState(link, "", link);
+
+					window.statenav.nav(link);
+				});
+			});
+		},
+
+        nav: function(link) {
+            window.statenav.container.trigger("sn-start-load");
+            $.ajax({
+                url: link,
+                dataType: "html",
+                method: "GET",
+                headers: {
+                    'X-SN': 'true',
+                    'X-PJAX': 'true',
+                    'X-SN-Container': window.statenav.container.selector,
+                    'X-PJAX-Container': window.statenav.container.selector
+                }
+            }).done(function (data) {
+                window.statenav.container.html(data);
+                window.statenav.container.trigger("sn-finish-load", [{ success: true }]);
+				window.statenav.attachElements();
+            }).fail(function (a, b, c) {
+                alert("Error de conexión con el servidor.");
+                console.log([a, b, c]);
+                window.statenav.container.trigger("sn-finish-load", [{ success: false, a: a, b: b, c: c }]);
+            });
+        }
+    };
+
+    window.onpopstate = function (event) {
+        console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+        window.statenav.nav(document.location);
+    };
+
+    if (typeof $ !== 'undefined') {
+        $(function () {
+            window.statenav.attachElements();
+        });
+    }
+
+})(jQuery);
